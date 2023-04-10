ee1c9794ac197bfccba7608c82cdcb5a42abaad3
ctkmpy <ahmadsaepudin032@gmail.com>
2023-04-10 10:12:36 +0700

    add first commits

---
+++ js/script.js
@@ -0,0 +1,91 @@
+// Selector Init 
+const body = document.querySelector('body');
+
+const aboutBtn = document.querySelector('.side-nav .about');
+const portfolioBtn = document.querySelector('.side-nav .portfolio');
+const galleryBtn = document.querySelector('.side-nav .gallery');
+const selector = document.querySelector('.side-nav .selector');
+
+const secAbout = document.querySelector('.sec-about');
+const secPortfolio = document.querySelector('.sec-portfolio');
+const secGallery = document.querySelector('.sec-gallery');
+
+
+// (Side Nav Click) to Selector Transition
+    
+    aboutBtn.addEventListener('click', function() {
+        aboutBtn.classList.add('selected');
+        portfolioBtn.classList.remove('selected');
+        galleryBtn.classList.remove('selected');
+        selector.classList.add('about-selected');
+        selector.classList.remove('portfolio-selected');
+        selector.classList.remove('gallery-selected');
+    });
+    portfolioBtn.addEventListener('click', function() {
+        aboutBtn.classList.remove('selected');
+        portfolioBtn.classList.add('selected');
+        galleryBtn.classList.remove('selected');
+        selector.classList.remove('about-selected');
+        selector.classList.add('portfolio-selected');
+        selector.classList.remove('gallery-selected');
+    });
+    galleryBtn.addEventListener('click', function() {
+        aboutBtn.classList.remove('selected');
+        portfolioBtn.classList.remove('selected');
+        galleryBtn.classList.add('selected');
+        selector.classList.remove('about-selected');
+        selector.classList.remove('portfolio-selected');
+        selector.classList.add('gallery-selected');
+    });
+
+
+// (Side Nav Click) to Smooth Scroll Content
+
+    aboutBtn.addEventListener('click', function() {
+        body.scrollTo({
+            top: secAbout.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+    portfolioBtn.addEventListener('click', function() {
+        body.scrollTo({
+            top: secPortfolio.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+    galleryBtn.addEventListener('click', function() {
+        body.scrollTo({
+            top: secGallery.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+
+// (Content Scroll) to Selector Transtition
+
+    body.addEventListener('scroll', function() {
+        if (body.scrollTop == secAbout.offsetTop){
+            aboutBtn.classList.add('selected');
+            portfolioBtn.classList.remove('selected');
+            galleryBtn.classList.remove('selected');
+            selector.classList.add('about-selected');
+            selector.classList.remove('portfolio-selected');
+            selector.classList.remove('gallery-selected'); 
+        }
+        else if (body.scrollTop == secPortfolio.offsetTop) {
+            aboutBtn.classList.remove('selected');
+            portfolioBtn.classList.add('selected');
+            galleryBtn.classList.remove('selected');
+            selector.classList.remove('about-selected');
+            selector.classList.add('portfolio-selected');
+            selector.classList.remove('gallery-selected');
+        }
+        else if (body.scrollTop == secGallery.offsetTop) {
+            aboutBtn.classList.remove('selected'); 
+            portfolioBtn.classList.remove('selected');
+            galleryBtn.classList.add('selected');
+            selector.classList.remove('about-selected');
+            selector.classList.remove('portfolio-selected');
+            selector.classList.add('gallery-selected');
+        }
+        console.log(secPortfolio.offsetTop);
+    });
