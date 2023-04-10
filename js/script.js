62ab50e92aaf49c2aa0edf2e0263a4ac9030bdaf
ctkmpy <ahmadsaepudin032@gmail.com>
2023-04-11 06:01:05 +0700

    fix scroll event for selector nav

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // Selector Init 
-const body = document.querySelector('body');
+const contentCont = document.querySelector('.content-container');
 
 const aboutBtn = document.querySelector('.side-nav .about');
 const portfolioBtn = document.querySelector('.side-nav .portfolio');
@@ -42,19 +42,19 @@ const secGallery = document.querySelector('.sec-gallery');
 // (Side Nav Click) to Smooth Scroll Content
 
     aboutBtn.addEventListener('click', function() {
-        body.scrollTo({
+        contentCont.scrollTo({
             top: secAbout.offsetTop,
             behavior: 'smooth'
         });
     });
     portfolioBtn.addEventListener('click', function() {
-        body.scrollTo({
+        contentCont.scrollTo({
             top: secPortfolio.offsetTop,
             behavior: 'smooth'
         });
     });
     galleryBtn.addEventListener('click', function() {
-        body.scrollTo({
+        contentCont.scrollTo({
             top: secGallery.offsetTop,
             behavior: 'smooth'
         });
@@ -62,8 +62,8 @@ const secGallery = document.querySelector('.sec-gallery');
 
 // (Content Scroll) to Selector Transtition
 
-    body.addEventListener('scroll', function() {
-        if (body.scrollTop == secAbout.offsetTop){
+    contentCont.addEventListener('scroll', function() {
+        if (contentCont.scrollTop == secAbout.offsetTop){
             aboutBtn.classList.add('selected');
             portfolioBtn.classList.remove('selected');
             galleryBtn.classList.remove('selected');
@@ -71,7 +71,7 @@ const secGallery = document.querySelector('.sec-gallery');
             selector.classList.remove('portfolio-selected');
             selector.classList.remove('gallery-selected'); 
         }
-        else if (body.scrollTop == secPortfolio.offsetTop) {
+        else if (contentCont.scrollTop < secGallery.offsetTop) {
             aboutBtn.classList.remove('selected');
             portfolioBtn.classList.add('selected');
             galleryBtn.classList.remove('selected');
@@ -79,7 +79,7 @@ const secGallery = document.querySelector('.sec-gallery');
             selector.classList.add('portfolio-selected');
             selector.classList.remove('gallery-selected');
         }
-        else if (body.scrollTop == secGallery.offsetTop) {
+        else if (contentCont.scrollTop >= secPortfolio.offsetTop) {
             aboutBtn.classList.remove('selected'); 
             portfolioBtn.classList.remove('selected');
             galleryBtn.classList.add('selected');
@@ -87,5 +87,4 @@ const secGallery = document.querySelector('.sec-gallery');
             selector.classList.remove('portfolio-selected');
             selector.classList.add('gallery-selected');
         }
-        console.log(secPortfolio.offsetTop);
     });
